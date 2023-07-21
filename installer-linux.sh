b7f8a824e0cac1a5e874437710f5aead1f90901e
KillaMeep <45155241+KillaMeep@users.noreply.github.com>
2023-07-20 19:43:29 -0600

    Create installer-linux.sh

---
+++ installer-linux.sh
@@ -0,0 +1,5 @@
+echo 'deb [trusted=yes] https://apt.fury.io/ascii-image-converter/ /' | sudo tee /etc/apt/sources.list.d/ascii-image-converter.list
+sudo apt update
+sudo apt install -y ascii-image-converter
+sudo apt install -y gifsicle
+sudo apt install -y python3-tk
